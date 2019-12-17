582b82c2aaccb57fc8a39c3c9accf46ac2d163fc
SERCAN BOYRAZ <sercan.boyraz@saglik.gov.tr>
2019-12-17 05:06:33 +0300

    deneme

---
+++ assets/js/topbar.js
@@ -0,0 +1,64 @@
+const {remote} = require('electron');
+const {BrowserWindow} = remote;
+let print_win;
+
+function navigateTo(url) {
+  document.querySelector('webview').src = url;
+}
+
+function getControlsHeight() {
+  let controls = document.querySelector('#controls');
+  if (controls) {
+    return controls.offsetHeight;
+  }
+  return 0;
+}
+
+function homeButton() {
+   document.querySelector('#home').onclick = () => {
+    let attribute = document.getElementById('webview');
+    let home = attribute.getAttribute("data-home");
+    navigateTo(home);
+  };
+}
+
+function iddaaButton() {
+  document.querySelector('#iddaa').onclick = () => {
+   let attribute = document.getElementById('webview');
+   let home = attribute.getAttribute("data-home");
+   navigateTo("https://www.iddaa.com");
+ };
+}
+
+function nesineButton() {
+  document.querySelector('#nesine').onclick = () => {
+   let attribute = document.getElementById('webview');
+   let home = attribute.getAttribute("data-home");
+   navigateTo("https://www.nesine.com");
+ };
+}
+
+function sahadanButton() {
+  document.querySelector('#sahadan').onclick = () => {
+   let attribute = document.getElementById('webview');
+   let home = attribute.getAttribute("data-home");
+   navigateTo("https://www.sahadan.com");
+ };
+}
+
+function milliPiyangoButton() {
+  document.querySelector('#milliPiyango').onclick = () => {
+   let attribute = document.getElementById('webview');
+   let home = attribute.getAttribute("data-home");
+   navigateTo("http://www.millipiyango.gov.tr/sonuclar/_cs_sayisal.php?&embedded=true");
+ };
+}
+
+function print() {
+  let webview = document.querySelector('webview');
+  print_win = new BrowserWindow({'auto-hide-menu-bar':true});
+  print_win.loadURL(webview.src);
+  print_win.webContents.on('did-finish-load', () => {
+    print_win.webContents.print();
+  });
+}
---
+++ assets/js/webview.js
@@ -0,0 +1,23 @@
+window.onresize = doLayout;
+
+onload = () => {
+  doLayout();
+  // Topbar functions
+  iddaaButton();
+  sahadanButton();
+  nesineButton();
+  milliPiyangoButton();
+  homeButton();
+  printButton();
+};
+
+function doLayout() {
+  let webview = document.querySelector('webview');
+  let windowWidth = document.documentElement.clientWidth;
+  let windowHeight = document.documentElement.clientHeight;
+  let controlsHeight = getControlsHeight();
+  let webviewHeight = windowHeight - controlsHeight;
+
+  webview.style.width = windowWidth + 'px';
+  webview.style.height = webviewHeight + 'px';
+}
